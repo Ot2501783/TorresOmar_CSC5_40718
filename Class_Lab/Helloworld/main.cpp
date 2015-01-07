1794c4805854a709ed3eb5b10f851d9a6ab8f739
ot2501783 <otorres21@student.rcc.edu>
2015-01-06 20:03:03 -0800

    Signed-off-by: ot2501783 <otorres21@student.rcc.edu>

---
+++ Class_Lab/Helloworld/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Omar Torres
+ * Purpose: Our first program
+ * Created on January 6, 2015, 6:05 PM
+ */
+
+ //System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+//Output Hello World
+    cout<<"Hello World"<<endl;
+    //Exit stage right!
+    return 0;
+}
+
