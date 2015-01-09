b13a0b16d4e0c629fd47846b8a4b86b53c1be2a0
ot2501783 <ot2501783@student.rcc.edu>
2015-01-08 18:41:39 -0800

    Signed-off-by: ot2501783 <ot2501783@student.rcc.edu>

---
+++ Assignment 1/Savitch_9thEd_Chap1_ProgProj1_1/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Omar Torres
+ * Created on January 8, 2015, 6:25 PM
+ * Purpose: Homework Assignment (Savitch_9thEd_Chap1ProgProj1)
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+//Declare Variables
+    int op1,op2,sum=0,prod=0;
+    //Prompt for the two operands
+    cout<<"input 2 integers having a range of [-2^31,"
+            cin>>op1>>op2
+            //Calculate the sum and the products
+            sum=sum+op1
+            sum=sum+op2
+            prod=prod*op1
+            prod=prod*op2
+            //Output the results 
+            cout<<op1"+"<<op2<<"="<<sum<<endl;
+            cout<<op1"+"<<op2<<"="<<prod<<endl;
+    return 0;
+}
+
