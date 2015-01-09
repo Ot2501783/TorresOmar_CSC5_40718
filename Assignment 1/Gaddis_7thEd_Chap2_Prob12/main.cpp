f2ec3041daa65d10213e01cec5d713df075cee96
ot2501783 <ot2501783@student.rcc.edu>
2015-01-08 20:15:01 -0800

    Signed-off-by: ot2501783 <ot2501783@student.rcc.edu>

---
+++ Assignment 1/Gaddis_7thEd_Chap2_Prob12/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Omar Torres
+ * Created on January 8, 2015, 7:45 PM
+ * Purpose: Homework Assignment (Gaddis_7thEd_Chap2_Prob12)
+ */
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+ //User Libraries
+  
+  //Global Constants 
+const unsigned short CVFTACS=43560;//Acre conversion 
+ 
+  //Function Prototypes
+  
+  // Execution Begins here
+
+
+int main(int argc, char** argv) {
+using namespace std;
+ //Declare Variables
+float inSqFt,outAcrs;
+//Prompt for input
+cout<<"Input the number of square feet"<<endl;
+cout<<"to convert to Acres"<<endl;
+cin>>inSqFt;
+//Calculate Acres
+outAcrs=inSqFt/CVFTACS;
+//Output the results
+cout<<inSqFt<<"(sq feet) = ";
+cout<<outAcrs<<"(acres)"endl;
+    return 0;
+}
--- Assignment 1/Gaddis_7thEd_Chap2_Prob17/main.cpp
+++ Assignment 1/Gaddis_7thEd_Chap2_Prob17/main.cpp
@@ -17,21 +17,21 @@ const char CONVPCT=100;//Percent Conversion
   //Function Prototypes
   
   // Execution Begins here
- 
 int main(int argc, char** argv) {
     //Declare and initialize variables 
     float  skPrice=2.177e1f;//Stock Price in $'s
     unsigned short nShares=600;//Number of shares of stocks
     char comish=2;//2 percent sales commission 
     //Calculate sales price, commission and total in $'s
-    float slProce=nShare*skPrice; 
+    float slPrice=nShares*skPrice; 
     float cmPrice=slPrice*comish/CONVPCT;
     float totPaid=slPrice+cmPrice
     //output results
-    cout<<fixed<<setprecision(2)<<showpoint<<
-    cout<<"Sales Price = $"<<slPrice<<endl;
-     cout<<"Commission = $"<<cmPrice<<endl;
-      cout<<"Total Paid = $"<<totPaid<<endl;
+    cout<<fixed<<setprecision(2)<<showpoint;
+    cout<<"Sales Price = $"<<setw(8)<<slPrice<<endl;
+    cout<<"Commission = $"<<setw(8)<<cmPrice<<endl;
+    cout<<"Total Paid = $"<<setw(8)<<totPaid<<endl;
+      //Exit Stage Right!
     return 0;
 }
 
