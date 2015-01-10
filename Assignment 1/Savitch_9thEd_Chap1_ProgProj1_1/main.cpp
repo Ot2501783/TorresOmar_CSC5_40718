6e857532a949bdf7a4a3372b34839206e821409a
ot2501783 <ot2501783@student.rcc.edu>
2015-01-09 21:03:08 -0800

    Signed-off-by: ot2501783 <ot2501783@student.rcc.edu>

--- Assignment 1/Savitch_9thEd_Chap1_ProgProj1_1/main.cpp
+++ Assignment 1/Savitch_9thEd_Chap1_ProgProj1_1/main.cpp
@@ -2,7 +2,7 @@
  * File:   main.cpp
  * Author: Omar Torres
  * Created on January 8, 2015, 6:25 PM
- * Purpose: Homework Assignment (Savitch_9thEd_Chap1ProgProj1)
+ * Purpose: Homework Assignment (Savitch_9thEd_Chap1_ProgProj1)
  */
 
 #include <iostream>
@@ -15,16 +15,16 @@ int main(int argc, char** argv) {
 //Declare Variables
     int op1,op2,sum=0,prod=1;
     //Prompt for the two operands
-    cout<<"input 2 integers"
-            cin>>op1 >>op2
+    cout<<"input 2 integers";
+            cin>>op1>>op2;
             //Calculate the sum and the products
-            sum=sum+op1
-            sum=sum+op2
-            prod=prod*op1
-            prod=prod*op2
+            sum=sum+op1;
+            sum=sum+op2;
+            prod=prod*op1;
+            prod=prod*op2;
             //Output the results 
-            cout<<op1"+"<<op2<<"="<<sum<<endl;
-            cout<<op1"+"<<op2<<"="<<prod<<endl;
+            cout<<op1 "+" <<op2<< "=" <<sum<<;
+            cout<<op1 "*" <<op2<< "=" <<prod<<; 
     return 0;
 }
 
