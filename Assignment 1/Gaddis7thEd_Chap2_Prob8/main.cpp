3c587c43635a62aa5d6e2742c3283c5bc51a9b66
ot2501783 <ot2501783@student.rcc.edu>
2015-01-10 21:26:07 -0800

    Signed-off-by: ot2501783 <ot2501783@student.rcc.edu>

---
+++ Assignment 1/Gaddis7thEd_Chap2_Prob8/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Omar Torres
+ * Created on January 10, 2015, 9:13 PM
+ * Purpose: Homework Assignment (Gaddis_7thEd_Chap2_Prob8)
+ */
+//system Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+  
+//Global Constants
+float slstx=.6//sales tax 6%
+
+//Function Prototypes
+  
+// Execution Begins here
+int main(int argc, char** argv) {
+//Declare variables 
+    float itm1=12.95;//in $ 
+    float itm2=54.92;//in $
+    float itm3=6.95;//in $
+    float itm4=14.95;//in $
+    float itm5=3.95;//in $
+    float sbttl, ttl;
+    //Display Items
+    cout<<" Razors "<< itm1 <<endl;
+    
+    return 0;
+}
+
