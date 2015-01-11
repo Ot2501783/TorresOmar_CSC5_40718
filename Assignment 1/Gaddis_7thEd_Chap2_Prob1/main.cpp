f5768e9a4f7303e35fa6ab0030093c8063c8952d
ot2501783 <ot2501783@student.rcc.edu>
2015-01-11 13:45:05 -0800

    Signed-off-by: ot2501783 <ot2501783@student.rcc.edu>

---
+++ Assignment 1/Gaddis_7thEd_Chap2_Prob1/main.cpp
@@ -0,0 +1,32 @@
+/* 
+ * File:   main.cpp
+ * Author: Omar Torres
+ * Created on January 11, 2015, 1:32 PM
+ * Purpose: Homework Assignment (Gaddis_7thEd_Chap2_Prob1)
+ */
+//system Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+  
+//Global Constants  
+
+//Function Prototypes
+  
+// Execution Begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    int op1=62, op2=99, ttl;
+    //Display numbers
+    cout<<op1<<endl;
+    cout<<op2<<endl;
+    //Add the two numbers
+    ttl=op1+op2;
+    //Display the sum
+    cout <<op1<<" + "<<op2<<" = "<<endl;
+    cout <<"Total "<< ttl <<endl;
+    
+    return 0;
+}
+
