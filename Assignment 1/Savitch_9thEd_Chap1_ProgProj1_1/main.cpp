1e979a700cf763fd87eec40278965b97b18168b6
ot2501783 <ot2501783@student.rcc.edu>
2015-01-10 19:43:33 -0800

    Signed-off-by: ot2501783 <ot2501783@student.rcc.edu>

--- Assignment 1/Savitch_9thEd_Chap1_ProgProj1_1/main.cpp
+++ Assignment 1/Savitch_9thEd_Chap1_ProgProj1_1/main.cpp
@@ -4,27 +4,32 @@
  * Created on January 8, 2015, 6:25 PM
  * Purpose: Homework Assignment (Savitch_9thEd_Chap1_ProgProj1)
  */
-
+//System Libraries
 #include <iostream>
 using namespace std;
 
-/*
- * 
- */
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
 int main(int argc, char** argv) {
-//Declare Variables
+    //Declare Variables
     int op1,op2,sum=0,prod=1;
     //Prompt for the two operands
-    cout<<"input 2 integers";
-            cin>>op1>>op2;
-            //Calculate the sum and the products
-            sum=sum+op1;
-            sum=sum+op2;
-            prod=prod*op1;
-            prod=prod*op2;
-            //Output the results 
-            cout<<op1 "+" <<op2<< "=" <<sum<<;
-            cout<<op1 "*" <<op2<< "=" <<prod<<; 
+    cout<<"Input 2 integers having a range of [-2^31,2^31-1]"<<endl;
+    cin>> op1>> op2;
+    //Calculate the sum and the products
+    sum=sum+op1;
+    sum=sum+op2;
+    prod=prod*op1;
+    prod=prod*op2;
+    //Output the results 
+    cout<<op1<<" + " <<op2<< " = "<<sum<<endl;
+    cout<<op1<<" * " <<op2<< " = "<<prod<<endl;
+              
     return 0;
 }
 
