5b5518718cb9787c710ba28c4346d75762e7cb48
ot2501783 <ot2501783@student.rcc.edu>
2015-01-10 20:49:07 -0800

    Signed-off-by: ot2501783 <ot2501783@student.rcc.edu>

---
+++ Assignment 1/Gaddis_7thEd_Chap2Prob14/main.cpp
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.cpp
+ * Author: Omar Torres
+ * Created on January 10, 2015, 8:47 PM
+ * Purpose: Homework Assignment (Gaddis_7thEd_Chap2_Prob14)
+ */
+//system Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+  
+//Global Constants  
+
+//Function Prototypes
+  
+// Execution Begins here
+int main(int argc, char** argv) {
+    cout<<" Name: Omar Torres \n Address: 712 La Bonita Ave, Perris, Ca 92571 \n Phone No: (760) 712-2668 \n Major: Electrical Engineering"<<endl; 
+    return 0;
+}
+
